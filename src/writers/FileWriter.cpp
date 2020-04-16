8cc7d552581b32679ed8e5604a42bd24e13b0944
Yury <gbalog@inbox.ru>
2020-04-16 14:43:59 +0500

    Missing header added

--- src/writers/FileWriter.cpp
+++ src/writers/FileWriter.cpp
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <cstring>
 #include <filesystem>
+#include <iomanip>
 //-----------------------------------------------------------------------------
 namespace fs = std::filesystem;
 //-----------------------------------------------------------------------------
